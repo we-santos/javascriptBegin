4075587adf115ed1aa70203fa33739f03e1b30e0
we-santos <we_santos@hotmail.com>
2018-03-10 01:42:13 -0300

    Chapter 2 - finish

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,29 @@
-var titulo = document.querySelector(".titulo");
+var paciente = document.querySelector("#primeiro-paciente");
 
-titulo.textContent = "Aparecida Nutricionista";
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if (peso >= 1000 || peso <= 0) {
+    console.log("Peso inválido!");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+}
+
+if (altura >= 3 || altura <= 0) {
+    console.log("Altura inválido!");
+    tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
