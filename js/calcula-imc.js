56bba3352c996202795ee44e799e2a9eda03dd00
we-santos <we_santos@hotmail.com>
2018-03-11 17:33:50 -0300

    Chapter 5 - finish

---
+++ js/calcula-imc.js
@@ -0,0 +1,40 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if (peso >= 1000 || peso <= 0) {
+        console.log("Peso inválido!");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    if (altura >= 3 || altura <= 0) {
+
+        console.log("Altura inválido!");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function calculaImc(peso, altura) {
+    var imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
+
